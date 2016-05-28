f84707461e7100d2015caccdb6cbbb73d65fe34f
brandonroycstn@hotmail.com <brandonroycstn@hotmail.com>
2016-05-27 19:10:34 -0600

    initial commit

---
+++ lab2/navbar.ascx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/**
+ * @Author Brandon Roy
+ * 
+ * */
+
+
+namespace lab2
+{
+    public partial class navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            setActivePage();
+        }
+
+        private void setActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+
+            }
+        }
+    }
+}
